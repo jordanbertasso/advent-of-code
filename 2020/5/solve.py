4c207644f4941ddbd771d5608df3d49110f06216
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2020-12-05 16:38:12 +1100

    Clean up

--- 2020/5/solve.py
+++ 2020/5/solve.py
@@ -2,29 +2,20 @@ passes = []
 with open('input.txt', 'r') as f:
     passes = list(map(str.strip, f.readlines()))
 
-def get_row(s, min=0, max=127):
+def get_pos(s, min, max, low_char, high_char):
     if s == '':
         return max
     
-    if s[0] == 'F':
-        return get_row(s[1:], min, (min+max)//2)
-    elif s[0] == 'B':
-        return get_row(s[1:], (min+max)//2, max)
-
-def get_col(s, min=0, max=7):
-    if s == '':
-        return max
-    
-    if s[0] == 'L':
-        return get_col(s[1:], min, (min+max)//2)
-    elif s[0] == 'R':
-        return get_col(s[1:], (min+max)//2, max)
+    if s[0] == low_char:
+        return get_pos(s[1:], min, (min+max)//2, low_char, high_char)
+    elif s[0] == high_char:
+        return get_pos(s[1:], (min+max)//2, max, low_char, high_char)
 
 
 ids = []
 for p in passes:
-    row = get_row(p[:7])
-    col = get_col(p[7:])
+    row = get_pos(p[:7], 0, 127, 'F', 'B')
+    col = get_pos(p[7:], 0, 7, 'L', 'R')
 
     id = row * 8 + col
     ids.append(id)
